ab09d3d1d37e43486df4de76831f8a612876a29f
Pawan Saini <pawan_tauras@yahoo.com>
2023-08-21 10:43:23 +0530

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -7,6 +8,16 @@ const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Realize Backend Server')
+    .setDescription('APIs description here')
+    .setVersion('0.1')
+    .addTag('realize')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
   app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
